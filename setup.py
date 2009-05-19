ebf2bd88bab4b7cc907e92beea9f2e729a9968d7
massimo.santini <massimo.santini@201233a8-134f-11de-a608-ad489a1f2375>
2009-05-19 12:05:13 +0000

    As uploaded 0.1a

--- setup.py
+++ setup.py
@@ -18,9 +18,25 @@ setup(
 	name = 'pyWebGraph',
 	version = '0.1a',
 	description = 'A bridge between WebGraph and Python/Jython',
+	long_description ="""This package is intended as a simple bridge easing the usage of the
+`WebGraph <http://webgraph.dsi.unimi.it/>`_ library in Python.
+
+This software is hosted at `<http://code.google.com/p/py-web-graph/>`_.
+
+Given that the library is written in Java, some modules of this package must
+be run with `Jython <http://www.jython.org/>`_, or a si milar Python
+interpreter able to execute Java code. To reduce the impact of such
+dependency, a basic XML-RPC server is provided to a llow any other Python
+interpreter to access WebGraph data.
+
+Finally, the package contains a simple wrapper for `Ubigraph
+<http://ubietylab.net/ubigraph/>`_ (a free tool for visualizing dynamic
+graphs) is provided, allowing some visual exploration of WebGraph data. """,
 	author = 'Massimo Santini',
 	author_email = 'santini@dsi.unimi.it',
 	url = 'http://py-web-graph.googlecode.com/',
 	packages = [ 'pywebgraph', 'pywebgraph.examples', 'pywebgraph.webgraph' ],
+	platforms = [ 'MacOS X', 'POSIX' ],
+	license = 'GNU General Public License (GPL)',
 	data_files = [ ( 'docs', [ 'COPYING', 'README.txt' ] ) ] + doc_files,
 )
